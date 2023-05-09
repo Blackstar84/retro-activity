2948357dafa129f629ef63c7f75ca3b1e50bfb51
Blackstar84 <carlosalmada84@gmail.com>
2023-05-09 13:14:00 -0400

    se quita el socket io

--- backend/app.js
+++ backend/app.js
@@ -35,18 +35,6 @@ app.use((error, req, res, next) => {
 });
 
 
-const socket = socketIo(server, {
-  cors: {
-    origin: '*',
-  },
-})
-
-socket.on('connection', (socket) => {
-  console.log('a user connected ' + socket.id, new Date()) // socket.id is unique for each connection
-})
-
-app.set('socket', socket)
-
 mongoose
 .connect('mongodb://localhost:27017/retro-activity')
 .then(()=>{
